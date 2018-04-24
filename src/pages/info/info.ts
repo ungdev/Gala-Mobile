0039ee961885f74f9760d637892922dea52f0dde
Arnaud <arnaud.dufour@utt.fr>
2018-04-24 20:07:39 +0200

    added spotify

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GMapPage } from '../gmap/gmap';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-info',
   templateUrl: 'info.html'
 })
 export class InfoPage {
+  public isIOS: boolean;
+  public selection: any;
+  public infosVisible: boolean;
+  public artistVisible: boolean;
+  public width: number;
+  public height: number;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController, public plt: Platform) {
+    this.isIOS = plt.is('ios')
+
+    this.selection = "info"
+    this.infosVisible = true;
+    this.artistVisible = false;
+    this.width = plt.width() - 35
+    this.height = plt.height() - 200
+    console.log(this.width + " " + this.height)
   }
 
   click(){
@@ -22,6 +37,23 @@ export class InfoPage {
     }
     if(event.direction == 4){
       this.navCtrl.parent.select(0);
+    }
+  }
+
+  showInfos(){
+    this.infosVisible = true
+    this.artistVisible = false
+  }
+  showArtist(){
+    this.infosVisible = false
+    this.artistVisible = true
+  }
+  ionSelect(){
+    if(this.selection == "info"){
+      this.showInfos()
+    }
+    else{
+      this.showArtist()
     }
   }
 }
