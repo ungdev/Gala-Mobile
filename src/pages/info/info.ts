28322747ad490bd72ec72b05c72aeba70d5d69d6
Arnaud <arnaud.dufour@utt.fr>
2018-05-06 12:25:21 +0200

    Changed map for the new version, adapted selected zones, added Sponsoring tab in info tab

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GMapPage } from '../gmap/gmap';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { Platform } from 'ionic-angular';
 
 @Component({
@@ -12,16 +13,18 @@ export class InfoPage {
   public selection: any;
   public infosVisible: boolean;
   public artistVisible: boolean;
+  public partenaireVisible: boolean;
   public width: number;
   public height: number;
 
 
-  constructor(public navCtrl: NavController, public plt: Platform) {
+  constructor(private iab: InAppBrowser, public navCtrl: NavController, public plt: Platform) {
     this.isIOS = plt.is('ios')
 
     this.selection = "info"
     this.infosVisible = true;
     this.artistVisible = false;
+    this.partenaireVisible = false;
     this.width = plt.width() - 35
     this.height = plt.height() - 200
     console.log(this.width + " " + this.height)
@@ -43,17 +46,67 @@ export class InfoPage {
   showInfos(){
     this.infosVisible = true
     this.artistVisible = false
+    this.partenaireVisible = false
   }
   showArtist(){
     this.infosVisible = false
     this.artistVisible = true
+    this.partenaireVisible = false
+  }
+  showPartenaire(){
+    this.infosVisible = false
+    this.artistVisible = false
+    this.partenaireVisible = true
   }
   ionSelect(){
     if(this.selection == "info"){
       this.showInfos()
     }
-    else{
+    else if(this.selection == "artists"){
       this.showArtist()
     }
+    else{
+      this.showPartenaire()
+    }
+  }
+
+  allysonLink(){
+    this.iab.create('http://ae-allyson.com', '_system', {});
+  }
+  amnestyLink(){
+    this.iab.create('https://www.amnesty.fr', '_system', {});
+  }
+  nrjLink(){
+    this.iab.create('http://www.nrj.fr', '_system', {});
+  }
+  socotecLink(){
+    this.iab.create('http://www.socotec.fr', '_system', {});
+  }
+  festilightLink(){
+    this.iab.create('http://www.festilight.com', '_system', {});
+  }
+  bdeLink(){
+    this.iab.create('https://www.facebook.com/bde.utt/', '_system', {});
+  }
+  k2aLink(){
+    this.iab.create('https://www.k2a-club.com', '_system', {});
+  }
+  mgelLink(){
+    this.iab.create('https://www.mgel.fr', '_system', {});
+  }
+  rgLink(){
+    this.iab.create('https://www.facebook.com/rivegauchecafe/?rf=125308984192319', '_system', {});
+  }
+  premautoLink(){
+    this.iab.create('http://www.premiumautomobiles-troyes.com', '_system', {});
+  }
+  socgeneraleLink(){
+    this.iab.create('https://www.societegenerale.fr', '_system', {});
+  }
+  teklissLink(){
+    this.iab.create('http://www.tekliss.com', '_system', {});
+  }
+  uttLink(){
+    this.iab.create('http://www.utt.fr/fr/index.html', '_system', {});
   }
 }
